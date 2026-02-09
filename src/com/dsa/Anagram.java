b09da32b04caf19f5e2d17100016dc0ef78996c6
athulatwork <blueblackathul@gmail.com>
2026-02-09 21:56:58 +0530

    feat(feat): added Queue using two stack

--- src/com/dsa/Anagram.java
+++ src/com/dsa/Anagram.java
@@ -8,18 +8,11 @@ public class Anagram {
         String y = "listen";
         char[] a = s.toCharArray();
         char[] b = y.toCharArray();
-        for (int i = 0; i < a.length; i++) {
-            for (int j = 1; j < a.length; j++) {
-                if (a[j] < a[j - 1]) {
-                    char temp = a[j];
-                    a[j] = a[j - 1];
-                    a[j - 1] = temp;
-                }
-            }
-        }
+       
+
 
         // Sort array b
-        for (int i = 0; i < b.length; i++) {
+        /*for (int i = 0; i < b.length; i++) {
             for (int j = 1; j < b.length; j++) {
                 if (b[j] < b[j - 1]) {
                     char temp = b[j];
@@ -27,7 +20,31 @@ public class Anagram {
                     b[j - 1] = temp;
                 }
             }
+        }*/
+         for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a.length-1; j++) {
+                if (a[j] > a[j + 1]) {
+                    char temp = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = temp;
+                }
+            }
         }
 
+
+        // Sort array b
+        for (int i = 0; i < b.length-1; i++) {
+            for (int j = 0; j < b.length - 1; j++) {
+                if (b[j] > b[j + 1]) {
+                    char temp = b[j];
+                    b[j] = b[j + 1];
+                    b[j + 1] = temp;
+                }
+            }
+        }
+       
+
         System.out.println(Arrays.toString(a));
+        System.out.println(Arrays.toString(b));
+        System.out.println(Arrays.equals(a, b));
     }}
--- src/com/dsa/MergeArray.java
+++ src/com/dsa/MergeArray.java
@@ -1,11 +1,14 @@
 package com.dsa;
 
+import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class MergeArray {
     public static void main(String[] args) {
         int[] arr1 = {10,9,32,5,7,31,4};
         int[] arr2={11,9,13,9,8,6,8};
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
         int[] arr =new int[arr1.length + arr2.length];
         for(int i =0 ; i<= arr.length -1 ;i++){
             if(i<arr1.length){
